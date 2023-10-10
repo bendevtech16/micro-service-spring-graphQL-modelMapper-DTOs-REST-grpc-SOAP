395f82694b6522f392193cec499fe3349f0dd245
benjamin OHANDJA <bendevtech16@gmail.com>
2023-10-10 09:01:59 +0100

    start dev

---
+++ src/main/java/cm/uy1/custumerservice/CustumerServiceApplication.java
@@ -0,0 +1,13 @@
+package cm.uy1.custumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustumerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cm/uy1/custumerservice/CustumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.uy1.custumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
